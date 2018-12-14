6c4b36abb4fe4a08b0f55d8fded575c07c013a37
Andrew Smith <andrew@andrew.codes>
2018-12-14 16:20:29 -0500

    refactor: change how modules are exported, include license and publish canary

---
+++ packages/components/src/index.js
@@ -0,0 +1,4 @@
+export { default as SpacedGroup } from './SpacedGroup';
+export { default as StyleProvider } from './StyleProvider';
+export * from './StyleContainer';
+export * from './WithBreakpoint';
