6d5e142bebe55b70b9a17144b4068511f2c66a5a
Andrew Smith <andrew@andrew.codes>
2018-12-17 16:20:53 -0500

    Enable publishing script and do not build site on publish; netlify handles this

--- scripts/bin/docker-publish.sh
+++ scripts/bin/docker-publish.sh
@@ -5,12 +5,8 @@ yarn bootstrap
 
 NODE_ENV=production
 yarn build
-BASE_URL=component-library yarn docz build
 
 TYPE=${PUBLISH_TYPE-patch}
 
-echo "Publishing ${TYPE} to NPM...."
-echo "yarn lerna publish ${PUBLISH_TYPE} --yes"
-
-echo "Publishing docs..."
-echo "yarn gh-pages -d ./.docz/dist"
+echo "Publishing ${TYPE} release to NPM...."
+yarn lerna publish ${PUBLISH_TYPE} --yes
