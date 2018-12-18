04b1902f4f1dec55b07ea0c1eeacc7cadbee858f
Andrew Smith <andrew@andrew.codes>
2018-12-18 07:47:41 -0500

    automate npm login for publishing in CI

--- scripts/bin/publish.sh
+++ scripts/bin/publish.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+docker run -e NPM_USER=$NPM_USER -e NPM_PASS=$NPM_PASS -e NPM_EMAIL=$NPM_EMAIL bravissimolabs/generate-npm-authtoken > ~/.npmrc
+
 [[ "$OSTYPE" == *"win"* || "$OSTYPE" == "msys" ]] && MOUNT="/$PWD" || MOUNT=$PWD
 
 docker run -it --rm --name publish -v "$MOUNT":/app node:10-stretch bash -c "cd app &&  ./scripts/bin/docker-publish.sh"
