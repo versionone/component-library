ec87fa1711cf94c5f26f367990c8b5a028925576
Andrew Smith <andrew@andrew.codes>
2018-12-18 08:16:34 -0500

    Do not remove proptypes in prod mode for docz site

--- doczrc.js
+++ doczrc.js
@@ -49,13 +49,6 @@ export default {
       '@babel/plugin-proposal-object-rest-spread',
       '@babel/plugin-syntax-dynamic-import',
       'babel-plugin-transform-react-fela-display-name',
-      [
-        'babel-plugin-transform-react-remove-prop-types',
-        {
-          mode: 'wrap',
-          ignoreFilenames: ['node_modules'],
-        },
-      ],
       'babel-plugin-dev-expression',
       [
         '@versionone/babel-plugin-react-docgen',
