3a38f80e4bb1bbd45f6ff0a8427ed4cf6bdc064e
Andrew Smith <andrew@andrew.codes>
2018-12-19 07:07:21 -0500

    chore: include env variable comment block to shell script files

--- scripts/bin/publish-docs.sh
+++ scripts/bin/publish-docs.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# ---- Required ENV variables ----
+# NETLIFY_TOKEN
+# GITHUB_USER
+# GITHUB_TOKEN
+# SHA (commit sha being published)
+# ----
+
 [[ "$OSTYPE" == *"win"* || "$OSTYPE" == "msys" ]] && MOUNT="/$PWD" || MOUNT=$PWD
 
 docker run -it --rm --name publish-docs -e NETLIFY_TOKEN=$NETLIFY_TOKEN -e GITHUB_USER=$GITHUB_USER -e GITHUB_TOKEN=$GITHUB_TOKEN -e SHA=$SHA -v "$MOUNT":/app node:10-stretch bash -c "cd app &&  ./scripts/bin/docker-publish-docs.sh"
