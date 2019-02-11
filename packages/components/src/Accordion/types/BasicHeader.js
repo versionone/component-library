7cc0cbe998a25130f5b96424c5de3c11625caef8
Andrew Smith <andrew@andrew.codes>
2019-02-11 08:14:24 -0500

    D-14522 formatting, lint error fixes, and named exports on Accordion

--- packages/components/src/Accordion/types/BasicHeader.js
+++ packages/components/src/Accordion/types/BasicHeader.js
@@ -4,61 +4,61 @@ import { StyleProvider, createComponent } from '../../StyleProvider';
 import { SpacedGroup } from '../../SpacedGroup';
 
 const Container = createComponent(
-	() => ({
-		padding: '4px 8px',
-		display: 'flex',
-		'flex-direction': 'row',
-		flexWrap: 'nowrap',
-		'justify-content': 'space-between',
-		'align-items': 'center',
-	}),
-	'div',
+  () => ({
+    padding: '4px 8px',
+    display: 'flex',
+    'flex-direction': 'row',
+    flexWrap: 'nowrap',
+    'justify-content': 'space-between',
+    'align-items': 'center',
+  }),
+  'div',
 );
 
 const Text = createComponent(
-	() => ({
-		display: 'flex',
-		textAlign: 'left',
-		flexDirection: 'column',
-		cursor: 'pointer',
-	}),
-	'div',
+  () => ({
+    display: 'flex',
+    textAlign: 'left',
+    flexDirection: 'column',
+    cursor: 'pointer',
+  }),
+  'div',
 );
 
 const BasicHeader = ({ title, secondaryText, supportingVisual, arrow }) => {
-	return (
-		<StyleProvider>
-			<Container>
-				<SpacedGroup xs={2} center>
-					{supportingVisual}
-					<Text>
-						<span>{title}</span>
-						<span>{secondaryText}</span>
-					</Text>
-				</SpacedGroup>
-				<div>{arrow}</div>
-			</Container>
-		</StyleProvider>
-	);
+  return (
+    <StyleProvider>
+      <Container>
+        <SpacedGroup xs={2} center>
+          {supportingVisual}
+          <Text>
+            <span>{title}</span>
+            <span>{secondaryText}</span>
+          </Text>
+        </SpacedGroup>
+        <div>{arrow}</div>
+      </Container>
+    </StyleProvider>
+  );
 };
 
 BasicHeader.propTypes = {
-	/**
-	 * Title of the Accordion Header
-	 */
-	title: PropTypes.string,
-	/**
-	 * Additional information about the Accordion Header
-	 */
-	secondaryText: PropTypes.string,
-	/**
-	 * Supporting visual
-	 */
-	supportingVisual: PropTypes.node,
-	/**
-	 * Arrow instance used to help visually indicate open or closed
-	 */
-	arrow: PropTypes.node,
+  /**
+   * Title of the Accordion Header
+   */
+  title: PropTypes.string,
+  /**
+   * Additional information about the Accordion Header
+   */
+  secondaryText: PropTypes.string,
+  /**
+   * Supporting visual
+   */
+  supportingVisual: PropTypes.node,
+  /**
+   * Arrow instance used to help visually indicate open or closed
+   */
+  arrow: PropTypes.node,
 };
 
 export default BasicHeader;
