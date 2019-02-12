0ebc1240804a539d4556f54044f7b8e5e8082167
Andrew Smith <andrew@andrew.codes>
2019-02-12 16:16:24 -0500

    S-67433 enable API props table for BreadcrumbItem

--- packages/components/src/Breadcrumb/Breadcrumb.js
+++ packages/components/src/Breadcrumb/Breadcrumb.js
@@ -1,62 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Fragment } from 'react';
-import { createComponent, styleUtils, StyleProvider } from '../StyleProvider';
+import { BreadcrumbItem } from './BreadcrumbItem';
+import { createComponent, StyleProvider } from '../StyleProvider';
 import { SpacedGroup } from '../SpacedGroup';
-import { getComponentDisplayName } from '../utils';
-
-const PsuedoLink = createComponent(
-  ({ selected, theme }) => ({
-    display: 'flex',
-    'align-items': 'center',
-    ...styleUtils.conditionalStyle(selected, 'cursor', 'default', 'pointer'),
-    ...styleUtils.conditionalStyle(
-      selected,
-      'color',
-      theme.Breadcrumb.selected,
-      theme.Breadcrumb.main,
-    ),
-    ':focus': {
-      ...theme.focused,
-    },
-    ':hover': {
-      ...styleUtils.conditionalStyle(
-        !selected,
-        'color',
-        theme.Breadcrumb.mainHighlight,
-      ),
-    },
-    '> a': {
-      'text-decoration': 'none',
-      ...styleUtils.conditionalStyle(selected, 'cursor', 'default', 'pointer'),
-      ...styleUtils.conditionalStyle(
-        selected,
-        'color',
-        theme.Breadcrumb.selected,
-        theme.Breadcrumb.main,
-      ),
-      ':focus': {
-        ...theme.focused,
-      },
-    },
-    '> a:hover:visited': {
-      ...styleUtils.conditionalStyle(
-        !selected,
-        'color',
-        theme.Breadcrumb.mainHighlight,
-      ),
-    },
-    '> a:visited': {
-      ...styleUtils.conditionalStyle(
-        selected,
-        'color',
-        theme.Breadcrumb.selected,
-        theme.Breadcrumb.main,
-      ),
-    },
-  }),
-  'span',
-  ['onClick'],
-);
 
 const Separator = createComponent(
   ({ theme }) => ({
@@ -64,77 +10,6 @@ const Separator = createComponent(
   }),
   'span',
 );
-
-const Space = createComponent(
-  () => ({
-    display: 'inline-block',
-    width: 4,
-  }),
-  'span',
-);
-
-const BreadcrumbItem = props => {
-  const icon = props.icon && (
-    <Fragment>
-      {React.createElement(props.icon, { size: 12 })}
-      {props.children && <Space />}
-    </Fragment>
-  );
-
-  const coloredChildren = React.Children.toArray(props.children).map(
-    (child, i) => {
-      return typeof child === 'string' ? (
-        <PsuedoLink key={i} selected={props.selected} onClick={props.onClick}>
-          {icon}
-          {child}
-        </PsuedoLink>
-      ) : getComponentDisplayName(child.type) === 'Link' ? (
-        <PsuedoLink key={i} selected={props.selected}>
-          {icon}
-          <a href={child.props.href}>{child.props.children}</a>
-        </PsuedoLink>
-      ) : (
-        <PsuedoLink key={i} selected={props.selected}>
-          {icon}
-          {child}
-        </PsuedoLink>
-      );
-    },
-  );
-
-  return props.collapse ? (
-    <PsuedoLink>...</PsuedoLink>
-  ) : props.children ? (
-    coloredChildren
-  ) : (
-    <PsuedoLink selected={props.selected}>{icon}</PsuedoLink>
-  );
-};
-
-BreadcrumbItem.propTypes = {
-  /**
-   * Content of breadcrumb
-   */
-  children: PropTypes.node,
-  /**
-   * If true this breadcrumb is selected
-   */
-  selected: PropTypes.bool,
-  /**
-   * If true the breadcrumb is replaced with ellipsis
-   */
-  collapse: PropTypes.bool,
-  /**
-   * Function that is called when breadcrumb is clicked
-   */
-  onClick: PropTypes.func,
-};
-
-BreadcrumbItem.defaultProps = {
-  selected: false,
-  collapse: false,
-  onClick: () => {},
-};
 
 class Breadcrumb extends React.Component {
   render() {
@@ -162,14 +37,11 @@ class Breadcrumb extends React.Component {
     );
   }
 }
-
-Breadcrumb.Item = BreadcrumbItem;
-
 Breadcrumb.propTypes = {
   /**
    * Collection Breadcrumb.Items
    */
-  children: PropTypes.array,
+  children: PropTypes.arrayOf(PropTypes.instanceOf(BreadcrumbItem)),
   /**
    * Separator character
    */
