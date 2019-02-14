49da1d0e77e264ea68a0cf05d4ddd1c0146043ca
Andrew Smith <andrew@andrew.codes>
2019-02-14 15:02:55 -0500

    Does this deploy props table?

--- scripts/bin/docker-install.sh
+++ scripts/bin/docker-install.sh
@@ -3,9 +3,9 @@
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
 if [ $PROD ]; then
-  yarn install --frozen-lockfile
+  yarn --frozen-lockfile
 else
-  yarn install
+  yarn
 fi
 
 yarn bootstrap
