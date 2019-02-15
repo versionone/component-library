824744998f54ff1e9f9d3c03b3a556d3b5d6c953
Andrew Smith <andrew@andrew.codes>
2019-02-15 07:05:47 -0500

    Lint but do not fail build

--- scripts/bin/lint.sh
+++ scripts/bin/lint.sh
@@ -3,4 +3,4 @@
 [[ "$OSTYPE" == *"win"* || "$OSTYPE" == "msys" ]] && MOUNT="/$PWD" || MOUNT=$PWD
 ${BASH_SOURCE%/*}/stop.sh
 
-docker run --rm --name lint --workdir="/app" -v "$MOUNT":/app cypress/base:10 bash -c "yarn lint"
+docker run --rm --name lint --workdir="/app" -v "$MOUNT":/app cypress/base:10 bash -c "./scripts/bin/docker-lint.sh"
