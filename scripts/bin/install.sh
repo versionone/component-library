6d45edd7f6765d10f28f09b3fb2b932c4f942206
Andrew Smith <andrew@andrew.codes>
2019-02-16 11:42:53 -0500

    Remove docker related scripts

--- scripts/bin/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-[[ "$OSTYPE" == *"win"* || "$OSTYPE" == "msys" ]] && MOUNT="/$PWD" || MOUNT=$PWD
-${BASH_SOURCE%/*}/stop.sh
-
-docker run --rm --name install --workdir="/app" -v "$MOUNT":/app node:10-stretch bash -c "./scripts/bin/docker-install.sh"
