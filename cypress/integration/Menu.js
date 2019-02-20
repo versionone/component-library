b640707d3871ce3aa53b10b4e458f3b3d1288b12
Andrew Smith <andrew@andrew.codes>
2019-02-20 07:58:22 -0500

    Additional test for Menu positioning and scroll placement#29

--- cypress/integration/Menu.js
+++ cypress/integration/Menu.js
@@ -13,6 +13,40 @@ context('Menu', () => {
       .should('exist');
   });
 
+  specify('are positioned relative to their anchor when opened', () => {
+    cy.get('[data-component="IconButton"]')
+      .first()
+      .click();
+
+    cy.get('#REACT_PORTAL')
+      .find('[data-component="Menu"]')
+      .first()
+      .then(el => {
+        expect(JSON.stringify(el[0].getBoundingClientRect())).to.eql(
+          JSON.stringify({
+            x: 1226,
+            y: 32,
+            width: 200,
+            height: 125,
+            top: 32,
+            right: 1426,
+            bottom: 157,
+            left: 1226,
+          }),
+        );
+      });
+  });
+
+  specify('opening does not reset scroll position to top of page', () => {
+    cy.get('[data-test="positioning"]')
+      .find('[data-component="IconButton"]')
+      .last()
+      .scrollIntoView()
+      .click();
+
+    cy.window().then(win => expect(win.scrollY).to.not.eql(0));
+  });
+
   specify(
     'clicking outside an open menu closes the menu and returns focus to element that triggered it to open',
     () => {
