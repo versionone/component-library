af8c295e890ffc65fafa9b250c2e8da658fdaf5a
Andrew Smith <andrew@andrew.codes>
2019-02-25 09:20:37 -0500

    Tests were matching to be run twice

--- scripts/tasks/test.js
+++ scripts/tasks/test.js
@@ -2,7 +2,7 @@ const shell = require('shelljs');
 const cypress = require('cypress');
 const updateStatus = require('../updateStatus');
 
-const exp = /localhost/;
+const exp = /localhost:3000$/;
 const status = {
   description: 'Cypress Tests',
   context: 'Verify/Testing',
@@ -11,52 +11,61 @@ const status = {
 updateStatus({
   ...status,
   state: 'pending',
-}).then(
-  () =>
-    new Promise((resolve, reject) => {
-      shell.exec(`yarn add -W --dev cypress`);
-      const child = shell.exec('yarn start', { async: true });
-      child.stdout.on('data', data => {
-        if (exp.test(data)) {
-          cypress
-            .run({
-              record: true,
-              key: process.env.CYPRESS_RECORD_KEY,
-            })
-            .then(({ runUrl, totalFailed, totalPassed }) => {
-              if (totalFailed === 0) {
-                updateStatus({
-                  ...status,
-                  state: 'success',
-                  description: `Passed: ${totalPassed}`,
-                  url: runUrl,
-                }).then(() => shell.exit(0));
-              } else {
-                updateStatus({
-                  ...status,
-                  description: `Failed: ${totalFailed}`,
-                  url: runUrl,
-                  state: 'failure',
-                }).then(() => shell.exit(1));
-              }
-            })
-            .catch(error => {
-              updateStatus({
-                ...status,
-                description: error,
-                state: 'error',
-              }).then(() => shell.exit(1));
-            });
-        }
-      });
-      child.on('exit', exitCode => {
-        if (exitCode > 0) {
+})
+  .then(
+    () =>
+      new Promise((resolve, reject) => {
+        shell.exec(`yarn add -W --dev cypress`);
+        const child = shell.exec('yarn start', { async: true });
+        child.stdout.on('data', data => {
+          if (exp.test(data)) {
+            resolve(true);
+          }
+        });
+        child.on('exit', exitCode => {
+          if (exitCode > 0) {
+            reject(exitCode);
+            return;
+          }
+          resolve(exitCode);
+        });
+      }),
+  )
+  .then(() =>
+    cypress
+      .run({
+        record: true,
+        key: process.env.CYPRESS_RECORD_KEY,
+      })
+      .then(({ runUrl, totalFailed, totalPassed }) => {
+        if (totalFailed === 0) {
           updateStatus({
             ...status,
-            description: `Exit code ${exitCode}`,
-            state: 'error',
-          }).then(() => shell.exit(exitCode));
+            state: 'success',
+            description: `Passed: ${totalPassed}`,
+            url: runUrl,
+          }).then(() => shell.exit(0));
+        } else {
+          updateStatus({
+            ...status,
+            description: `Failed: ${totalFailed}`,
+            url: runUrl,
+            state: 'failure',
+          }).then(() => shell.exit(1));
         }
-      });
-    }),
-);
+      })
+      .catch(error => {
+        updateStatus({
+          ...status,
+          description: error,
+          state: 'error',
+        }).then(() => shell.exit(1));
+      }),
+  )
+  .catch(exitCode =>
+    updateStatus({
+      ...status,
+      description: `Error: ${exitCode}`,
+      state: 'error',
+    }).then(() => shell.exit(exitCode)),
+  );
