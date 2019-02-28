8d1e741d760780f54a7ce23ae8448818270f3fc8
Andrew Smith <andrew@andrew.codes>
2019-02-28 17:56:02 -0500

    Clamp test passes
    Previous test relied on body font-size/line-height that is different than the new gatsby site. Clamp does not care what the height is, but due to the different styles, the height has changed.
    
    We really want to evaluate that the height is 1 line worth.

--- cypress/integration/Clamp.js
+++ cypress/integration/Clamp.js
@@ -5,10 +5,12 @@ context('Clamp', () => {
 
   specify('Content can be clamped to a single line', () => {
     cy.get('[data-test="clamp-text"]')
-      .should('have.css', 'height', '28px')
-      .should('have.css', 'width', '750px')
+      .should('have.css', 'width', '921px')
       .should('have.css', 'text-overflow', 'ellipsis')
       .should('have.css', 'overflow', 'hidden')
-      .should('have.css', 'white-space', 'nowrap');
+      .should('have.css', 'white-space', 'nowrap')
+      .then(el => {
+        expect(el.height()).to.be.closeTo(26, 4);
+      });
   });
 });
